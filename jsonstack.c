f56275d2a495a08a762c9f5f2d470461d46aef5e
ziheng <zihenglv@gmail.com>
2017-08-26 01:54:22 +0800

    modified jsonstack.c

--- jsonstack.c
+++ jsonstack.c
@@ -3,48 +3,77 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 //#include "leptjson.h"
 
-typedef int type;
+extern void* my_memcpy(void *dest_addr, const void *src_addr, size_t n ) {
+     char* dest = dest_addr;
+     const char* src  = src_addr;
+     while( n > 0 ) {
+        *dest++ = *src++;
+        n--;
+     }
+    return dest_addr;
+}
+
+typedef unsigned char byte;
 
 typedef struct _st {
-    type  *base;
+    byte  *base;
     size_t top;
     size_t size;
 } st;
 
 void init_stack(st *head) {
-    head->base  = (type*)malloc(sizeof(type)*10);
+    head->base  = (byte*)malloc(sizeof(byte)*1024);
     head->top   = 0;
-    head->size  = sizeof(type)*10;
+    head->size  = sizeof(byte)*1024;
 }
 
-void push_stack(st *head, type *e) {
-    memcpy((unsigned char*)head->base + head->top, e, sizeof(type));
-    head->top += sizeof(type);
+void push_stack(st *head, void *e, size_t s) {
+    while((head->size - head->top) < s) {
+        head->base = (byte*)realloc(head->base, head->size * 2);
+        head->size *= 2;
+    }
+    my_memcpy(head->base + head->top, e, s);
+    head->top += s;
 }
-type* pop_stack(st *head) {
+void* pop_stack(st *head, size_t s) {
     if(head->top == 0) {
         return NULL;
     }
-    head->top -= sizeof(type);
-    return (type*)((unsigned char*)head->base + head->top);
+    head->top -= s;
+    return (void*)(head->base + head->top);
 }
 
+void del_stack(st *head) {
+    if(head->top != 0) {
+    }
+    free(head->base);
+    head->size = 0;
+}
 #endif /* json_stack_h */
 
+#define TESTNUM 1024
 int main(void) {
+
     st s;
     int i;
     init_stack(&s);
-    for(i = 0; i<10; i++) {
-        push_stack(&s, &i);
+    void* p = malloc(TESTNUM*2);
+    push_stack(&s, p, TESTNUM*2);
+    printf("%p\n", pop_stack(&s, TESTNUM*2));
+    del_stack(&s);
+
+#if 0
+    for(i = 0; i<100; i++) {
+        double d = i;
+        push_stack(&s, &d, sizeof(d));
     }
-    for(i = 0; i<10; i++) {
-        printf("%d ", *(pop_stack(&s)));
+    for(i = 0; i<100; i++) {
+        printf("%.2lf ", *((double*)(pop_stack(&s, sizeof(double)))));
     }
     printf("\n");
-
+#endif
     return 0;
 }
